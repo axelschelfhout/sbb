9f0a0b92f529244e0f64792801552e586a4c587a
Anne Schunselaar <anne@lyfter.nl>
2026-05-07 18:12:43 +0200

    optimize header dropdown

--- wp-content/themes/sbb/resources/js/app.js
+++ wp-content/themes/sbb/resources/js/app.js
@@ -15,3 +15,25 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dropdowns = document.querySelectorAll('li.menu-item.menu-item-has-children');
+
+  if (dropdowns) {
+    dropdowns.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+
+            const dropdownMenu = link.querySelector('ul.sub-menu');
+
+            if (dropdownMenu) {
+              dropdownMenu.classList.toggle('is-open');
+
+              const isExpanded = menu.classList.contains('is-open');
+              toggleButton.setAttribute('aria-expanded', isExpanded);
+            }
+        });
+    });
+  }
+});
